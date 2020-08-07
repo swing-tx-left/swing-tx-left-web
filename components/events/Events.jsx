0391f5597cb18d000957c56ec74b4189f32d3c6a
E. Krebs <ekrebs@outlook.com>
2020-08-06 23:56:27 -0500

    revalidate events on page load

--- components/events/Events.jsx
+++ components/events/Events.jsx
@@ -17,7 +17,7 @@ export function Events(props){
 
 	
 
-	let obj=useSWR('https://api.mobilize.us/v1/organizations/210/events?timeslot_end=gte_now', getSwingLeftEvents,{initialData:props.eventData});
+	let obj=useSWR('https://api.mobilize.us/v1/organizations/210/events?timeslot_end=gte_now', getSwingLeftEvents,{initialData:props.eventData,revalidateOnMoun:true});
 	
 		
 		//fix id mess
