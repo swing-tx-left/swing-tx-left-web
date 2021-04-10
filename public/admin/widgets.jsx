0be3d0d3cd6f6b981bca2e1613ecaf52ffce62da
E. Krebs <ekrebs@outlook.com>
2021-04-09 23:23:38 -0500

    fixe widgets

--- public/admin/widgets.jsx
+++ public/admin/widgets.jsx
@@ -1,37 +1,43 @@
 import React from 'react'
 
-import {Editor} from '@tinymce/tinymce-react';
+import { Editor } from '@tinymce/tinymce-react';
 
+
+//cleanup
 export class SlugUnique extends React.Component {
 	constructor(props) {
 		super(props);
 		this.updateVal = this.updateVal.bind(this);
 		this.fillBasedOnTitle = this.fillBasedOnTitle.bind(this);
-		this.getNoOtherSlugs();
+		this.isValid=this.isValid.bind(this);
 		this.state = {
-			fillWithTitle: false
+			fillWithTitle: false,
+			slugs: []
 		}
-
+		this.getNoOtherSlugs();
 	}
 
 	async getNoOtherSlugs() {
-		let useful = JSON.parse(JSON.stringify(this.props))
-		let q = await this.props.query('slugsSearch', useful.entry.collection, ['slug'], '')
-		console.log('search');
-		console.log(this.props);
+		
+		let q = await this.props.query('slugsSearch', 'pages', ['slug'], '')
+		// 	console.log('search');
+		// console.log(this.props);
 		console.log(q.payload);
-		//console.log(q.payload?.response);
+		// 	//console.log(q.payload?.response);
+		this.setState({ slugs: q.payload.hits });
 
+		return q;
 
 	}
 
 	updateVal(event) {
 		this.props.onChange(event.target.value);
 		this.getNoOtherSlugs();
-
+		//console.log(this.state);
 
 	}
-	async fillBasedOnTitle() {
+	fillBasedOnTitle() {
+		console.log(this.props);
 		if (!this.state.fillWithTitle) {
 			this.setState({ fillWithTitle: true });
 			//force redraw of event to get new props
@@ -39,29 +45,31 @@ export class SlugUnique extends React.Component {
 		}
 		else {
 			this.setState({ fillWithTitle: false });
-			let useful = JSON.parse(JSON.stringify(this.props))
-			console.log(useful.entry.data.title)
-			this.props.onChange(useful.entry.data.title.toLowerCase().replace(/[^\w]/g, '-'));
+			try {
+				this.props.onChange(this.props.entry.toJSON().data.title.toLowerCase().replace(/[^\w]/g, '-'));
+			}
+			catch (e) {
+				alert(e)
+			}
 			this.getNoOtherSlugs();
 		}
 
 	}
-	componentDidUpdate(prevProps,prevState) {
-		//console.log('compup')
-	//	console.log(this)
-		if(this.state.fillWithTitle){
+	componentDidUpdate(prevProps, prevState) {
+		
+		if (this.state.fillWithTitle) {
 			this.fillBasedOnTitle();
 		}
 	}
-	isValid() {
-		let useful = JSON.parse(JSON.stringify(this.props));
-		console.log(useful)
+	isValid () {
+		
+		
 
-		console.log(useful.queryHits.slugsSearch)
-		if (useful.queryHits.slugsSearch.some((el) => {
+		
+		if (this.state.slugs.some((el) => {
 			console.log(this.props.entry.path)
 
-			return el.data.slug === this.props.value && useful.entry.path !== el.path;
+			return el.data.slug === this.props.value && this.props.entry.toJSON().path !== el.path;
 		})) {
 			return { error: { message: 'Chose a unique slug! This one already exists' } };
 		}
@@ -73,27 +81,21 @@ export class SlugUnique extends React.Component {
 		return false;
 
 	}
-	// console.log(props)
-	// return ( <pre>
-	// 	{}
-	// 	{
 
-	// 	}
-	// 	{JSON.stringify(props,null,'\t')}
-	// 	</pre>);
 	render() {
 		//
-		
+
 		console.log(this.props);
 		return (
 			<>
 
 				<input className={this.props.classNameWrapper} type="text" value={this.props.value} onChange={(e) => { this.updateVal(e) }} id={this.props.forID} />
 				{(this.props.value === '' || this.props.value === undefined) &&
-					<button onClick={(e)=>{
+					<button onClick={(e) => {
 						console.log('out')
 						console.log(this);
-						this.fillBasedOnTitle()}}>Fill BasedOn title</button>
+						this.fillBasedOnTitle()
+					}}>Fill BasedOn title</button>
 				}
 			</>
 		);
@@ -107,75 +109,76 @@ export function SlugUniquePrev(props) {
 
 
 
-export class HTMLEditor extends React.Component{
+export class HTMLEditor extends React.Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props)
 		//this.state={content:this.props.value};
-		this.blah=this.blah.bind(this);
+		this.blah = this.blah.bind(this);
 
 	}
-	blah(content,editor){
+	blah(content, editor) {
 		console.log(content);
 		console.log(this.props);
-	
+
 		this.props.onChange(content);
 	}
 
-	async getLinks(){
-		let useful = JSON.parse(JSON.stringify(this.props))
-		let q = await this.props.query('pagesSearch', useful.entry.collection, ['slug'], '')
+	async getLinks() {
+		let q = await this.props.query('', 'pages', ['slug'], '');
 		console.log('searchlinks');
 		console.log(this.props);
 		console.log(q.payload);
 		;
 		return [
-			...q.payload.response.hits.map((el)=>{
-				return {title:el.data.title,value:'/'+el.data.slug}
+			...q.payload.hits.map((el) => {
+				return { title: el.data.title, value: '/' + el.data.slug }
 			}),
-			{title:'Files',menu:[
-				...this.getFiles()
-			]},
-			
+			{
+				title: 'Files', menu: [
+					...this.getFiles()
+				]
+			},
+
 		]
 	}
-	getFiles(){
-		
+	getFiles() {
+
 
 		// console.log(await this.props.mediaPaths.get(this.props.config.media_folder));
 		//console.log(this.props.onOpenMediaLibrary());
 		// console.log(this.props.getAsset({path:'/'}));
 		console.log('files');
-		console.log(JSON.parse(JSON.stringify(this.props.entry.get('mediaFiles'))));
-		return JSON.parse(JSON.stringify(this.props.entry.get('mediaFiles'))).map((el)=>{
-				return {title:el.name,value:'/files/'+el.name}
+		console.log(this.props.entry.get('mediaFiles').toJSON());
+		return this.props.entry.get('mediaFiles').toJSON().map((el) => {
+			return { title: el.name, value: '/files/' + el.name }
 		});
-		
+
 	}
 
-	render(){
+	render() {
 		return (
 			<div>
-				<button onClick={()=>{this.props.onOpenMediaLibrary()}}>Show Media</button>
+				<button onClick={() => { this.props.onOpenMediaLibrary() }}>Show Media</button>
 				<Editor value={this.props.value} init={{
-					height:600,
-					convert_urls:false,
-					link_list:async (success)=>{
+					height: 600,
+					convert_urls: false,
+					link_list: async (success) => {
 						success(await this.getLinks())
 					},
-					image_list:(success)=>{
+					image_list: (success) => {
 						success(this.getFiles());
 					},
-					plugins:'image anchor table link lists advlists charmap emoticons wordcount fullscreen code' ,
-					toolbar:['undo redo | aligncenter alignjustify alignleft alignnone alignright | bold italic | numlist bullist | indent outdent | removeformat formatselect']
-				}}  outputFormat='html' onEditorChange={this.blah}/>
+					plugins: 'image anchor table link lists advlists charmap emoticons wordcount fullscreen code',
+					toolbar: ['undo redo | aligncenter alignjustify alignleft alignnone alignright | bold italic | numlist bullist | indent outdent | removeformat formatselect']
+				}} outputFormat='html' onEditorChange={this.blah} />
 
-		</div>	
+			</div>
 		);
 	}
 
 }
 
-export function HTMLPreview(props){
+export function HTMLPreview(props) {
 	return <div>TODO HTML PREVIEW</div>
 }
